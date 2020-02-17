0a7b074e3bb0f0f5c3f9bf7b283c729367ff7df4
myluoluo <admin@myluoluo.com>
2020-02-17 20:51:39 +0800

    add cctv

---
+++ source/app.here.cctv/index.js
@@ -0,0 +1,110 @@
+const http = require("http")
+const net = require("net")
+const pref = require("pref")
+const cache = require('cache')
+
+const TvList = 'cctv1,cctv2,cctv3,cctv4,cctv5,cctv6,cctv7,cctv8,cctv9,cctv10,cctv11,cctv12,cctv13,cctv14,cctv15,cctv5plus,cctv17,cctveurope,cctvamerica'.split(',')
+const selectTv = TvList[pref.get("tvName")];
+
+console.log("tvName: " + selectTv)
+console.log("skipOld: " + pref.get("skipOld"))
+
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+function callHere(tvList) {
+    let popList = []
+    let popIndex = 0;
+    for (var i = 0; i < tvList['list'].length; i++) {
+        let entry = tvList['list'][i]
+        if(pref.get("skipOld") == 'true' && new Date() > new Date(entry.endTime * 1000)) {
+            // console.log("跳过已结束项目：" + entry.title)
+            // console.log(new Date(entry.endTime * 1000).Format("yyyy-MM-dd hh:mm"))
+            continue
+        }
+        popIndex++
+        popList.push({
+            title: popIndex + ". " + entry.title,
+            accessory: {
+                title: entry.showTime
+            }
+        });
+    }
+
+    here.setMenuBar({ title: tvList['channelName'] + ": " + tvList['isLive'] })
+    // here.setDock({
+    //     title: tvList['isLive'],
+    //     detail: tvList['channelName']
+    // })
+
+    // Mini Window
+    here.setMiniWindow({
+        title: tvList['isLive'],
+        detail: tvList['channelName'],
+        accessory: {
+            title: new Date(tvList['liveSt'] * 1000).Format("hh:mm"),
+            detail: "开始时间"
+        },
+        popOvers: popList
+    })
+}
+
+function updateData() {
+    here.setMiniWindow({ title: "Updating…" })
+    let currentDate = new Date().Format("yyyyMMdd");
+
+    let cacheData = cache.get(selectTv + "-" + currentDate)
+    if(cacheData == undefined) {
+        // console.log('no cache!')
+        http.get(`https://api.cntv.cn/epg/getEpgInfoByChannelNew?c=${selectTv}&serviceId=tvcctv&d=${currentDate}&t=json`)
+        .then(function(response) {
+            if (response.data == undefined) {
+                return here.setMiniWindow({ title: "Invalid data." })
+            }
+
+            let tvList = response.data['data'][selectTv];
+            // console.log(JSON.stringify(tvList))
+
+            if (tvList['list'].length <= 0) {
+                return here.setMiniWindow({ title: "Entrylist is empty." })
+            }
+
+            cache.set(selectTv + "-" + currentDate, tvList)
+            callHere(tvList)
+        })
+        .catch(function(error) {
+            console.error(`Error: ${JSON.stringify(error)}`)
+            return here.setMiniWindow({ title: JSON.stringify(error) })
+        })
+    } else {
+        // console.log('cache data!')
+        callHere(JSON.parse(cacheData))
+    }
+}
+
+here.onLoad(() => {
+    cache.removeAll()
+    updateData()
+    setInterval(updateData, 233*1000);
+})
+
+net.onChange((type) => {
+    console.log("Connection type changed:", type)
+    cache.removeAll()
+    if (net.isReachable()) {
+        updateData()
+    }
+})
