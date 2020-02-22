d924bc3da760de613c7c530a59fc502b5a8d057f
Ostrichbeta <ostrichb@outlook.com>
2020-02-22 14:53:33 +0000

    emergency update

--- source/app.here.ostrichb.yandextranslator/zh-TW.js
+++ source/app.here.ostrichb.yandextranslator/zh-TW.js
@@ -17,7 +17,10 @@ function getText() {
         },
         "error": {
             "notext": "ˇ你的剪貼板中沒有文字",
-            "nonetwork": "沒有網路連線"
+            "nonetwork": "沒有網路連線",
+            "nokey": "沒有輸入您的翻譯API金鑰",
+            "keyerror": "您的API金鑰無效",
+            "tutorial": "請查閱這個頁面來獲得有關API金鑰的申請方法"
         }
     }
     return origin;
