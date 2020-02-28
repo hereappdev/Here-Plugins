4c23bd5c554e7832ba64f5c8959a3bb753c3cfbd
lifesign <leocloudy@gmail.com>
2020-02-28 15:27:27 +0800

    feat: add meta-news plugin

---
+++ source/Meta-News/template/index.js
@@ -0,0 +1,50 @@
+const _ = require("lodash")
+const net = require("net")
+
+function updateData() {
+    const LIMIT = 10
+    
+    here.setMiniWindow({ title: "Updating…" })
+    
+    here.parseRSSFeed("{{rssFeedUrl}}")
+    .then((feed) => {
+        if (feed.items.length <= 0) {
+            return here.setMiniWindow({ title: "No item found." })
+        }
+    
+        if (feed.items.length > LIMIT) {
+            feed.items = feed.items.slice(0, LIMIT)
+        }
+    
+        const topFeed = feed.items[0]
+    
+        // Mini Window
+        here.setMiniWindow({
+            onClick: () => { if (topFeed.link != undefined)  { here.openURL(topFeed.link) } },
+            title: topFeed.title,
+            detail: "{{miniDetail}}",
+            popOvers: _.map(feed.items, (item, index) => {
+                return {
+                    title: `${index + 1}. ${item.title}`,
+                    onClick: () => { if (item.link != undefined)  { here.openURL(item.link) } },
+                }
+            })
+        })
+    })
+    .catch((error) => {
+        console.error(`Error: ${JSON.stringify(error)}`)
+    })
+}
+
+here.onLoad(() => {
+    updateData()
+    // Update every 2 hours
+    setInterval(updateData, 2*3600*1000);
+})
+
+net.onChange((type) => {
+    console.log("Connection type changed:", type)
+    if (net.isReachable()) {
+        updateData()
+    }
+})
