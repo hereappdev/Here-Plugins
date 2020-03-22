eeee1f6ddc3dc0ddbf515960001043c1205423b4
Here App Dev <HereDotApp@gmail.com>
2020-03-23 00:00:22 +0800

    feat(debug): added support for tabs style

--- source/Debug/index.js
+++ source/Debug/index.js
@@ -8,7 +8,7 @@ function updateData() {
     if (keys.includes("uuid")) {
         keys.splice(keys.indexOf("uuid"), 1)
     }
-    let popovers = _.map(keys, (key) => {
+    let versionData = _.map(keys, (key) => {
         let val = versions[key]
         return {
             title: key,
@@ -17,22 +17,23 @@ function updateData() {
             }
         }
     })
-    popovers.push({ 
+    let debug = []
+    debug.push({ 
         title: "Reveal Logs in Finder…",
         onClick: () => { process.openLogsFolder() }
     })
-    popovers.push({ 
+    debug.push({ 
         title: "Reveal Plugins in Finder…",
         onClick: () => { process.openPluginsFolder() }
     })
     if (typeof(process.checkForUpdates) === "function") { 
         // checkForUpdates function exists
-        popovers.push({
+        debug.push({
             title: "Check for Updates…",
             onClick: () => { process.checkForUpdates() }
         })
     }
-    popovers.push({ 
+    debug.push({ 
         title: `Toggle auto install preloaded plugins: ${process.installPreloadPluginsAtLaunch()}`,
         onClick: () => { 
             process.toggleInstallPreloadPluginsAtLaunch() 
@@ -57,7 +58,16 @@ function updateData() {
 
     // Popovers
     if (typeof(here.popover.set) == "function") {
-        here.popover.set(popovers)
+        here.popover.set([
+            {
+                title: "Version",
+                data: versionData
+            },
+            {
+                title: "Debug",
+                data: debug
+            }
+        ])
     }
 
     // Menu Bar
