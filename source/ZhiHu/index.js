5090f1ade7a2735d6e4e093133eb39b2d0c1f808
Here App Dev <HereDotApp@gmail.com>
2020-03-24 02:48:58 +0800

    test popovers tabs

--- source/ZhiHu/index.js
+++ source/ZhiHu/index.js
@@ -2,60 +2,131 @@ const _ = require("underscore")
 const http = require("http")
 const net = require("net")
 
+function getDate(api, LIMIT = 15) {
+
+    let entryList = []
+    return http.get("https://www.zhihu.com/api/v3/feed/topstory/hot-lists/" + api + "?limit=" + LIMIT)
+        .then(function (response) {
+
+            const json = response.data
+            entryList = json.data
+
+            if (entryList == undefined) {
+                return here.miniWindow.set({ title: "Invalid data." })
+            }
+
+            if (entryList.length <= 0) {
+                return here.miniWindow.set({ title: "Entrylist is empty." })
+            }
+
+            if (entryList.length > LIMIT) {
+                entryList = entryList.slice(0, LIMIT)
+            }
+
+            entryList = Object.keys(entryList).map(function (key, index) {
+                entryList[key].title = entryList[key]["target"]["title"]
+                entryList[key].url = entryList[key]["target"]["url"].replace("api.zhihu.com/questions", "www.zhihu.com/question")
+                return entryList
+            });
+
+            return entryList
+        })
+}
+
 function updateData() {
-    const LIMIT = 10
 
     here.miniWindow.set({ title: "Updating…" })
-    http.get(`https://www.zhihu.com/api/v3/explore/guest/feeds?limit=${LIMIT}`)
-    .then(function(response) {
-        const json = response.data
-        let entryList = json.data
-        if (entryList == undefined) {
-            return here.miniWindow.set({ title: "Invalid data." })
-        }
-    
-        if (entryList.length <= 0) {
-            return here.miniWindow.set({ title: "Entrylist is empty." })
-        }
-    
-        if (entryList.length > LIMIT) {
-            entryList = entryList.slice(0, LIMIT)
-        }
-    
-        entryList = _.map(entryList, (entry) => {
-            entry.title = entry["target"]["question"]["title"]
-            entry.url = entry["target"]["question"]["url"].replace("api.zhihu.com/questions", "www.zhihu.com/question")
-            return entry
-        })
-    
-        const topFeed = entryList[0]
+
+    Promise.all([
+        getDate("total"),
+        getDate("science"),
+        getDate("digital"),
+        getDate("sport"),
+        getDate("fashion"),
+        getDate("film")
+    ]).then(function (values) {
+
+        const topFeed = values[0][0][0]["target"]
+
         // Mini Window
         here.miniWindow.set({
-            onClick: () => { if (topFeed.url != undefined)  { here.openURL(topFeed.url) } },
-            title: topFeed.title,
+            onClick: () => { if (topFeed["id"] != undefined) { here.openURL("https://www.zhihu.com/question/" + topFeed["id"]) } },
+            title: topFeed["title"],
             detail: "知乎热榜"
         })
-        here.menuBar.set({
-            title: topFeed.title
-        })
-        here.popover.set(_.map(entryList, (entry, index) => {
-            return {
-                title: entry.title,
-                onClick: () => { if (entry.url != undefined)  { here.openURL(entry.url) } }
-            }
-        }))
 
-    })
-    .catch(function(error) {
-        console.error(`Error: ${JSON.stringify(error)}`)
-        here.miniWindow.set({ title: JSON.stringify(error) })
-    })
+        here.menuBar.set({
+            title: topFeed["title"]
+        })
+
+        let tabs = [
+            {
+                title: "全部",
+                data: _.map(values[0], (entry, index) => {
+                    // console.log(entry[index]["target"]["title"])
+                    return {
+                        title: entry[index]["target"]["title"],
+                        onClick: () => { if (entry[index]["target"]["id"] != undefined) { here.openURL("https://www.zhihu.com/question/" + entry[index]["target"]["id"]) } }
+                    }
+                })
+            },
+            {
+                title: "科学",
+                data: _.map(values[1], (entry, index) => {
+                    // console.log(entry[index]["target"]["title"])
+                    return {
+                        title: entry[index]["target"]["title"],
+                        onClick: () => { if (entry[index]["target"]["id"] != undefined) { here.openURL("https://www.zhihu.com/question/" + entry[index]["target"]["id"]) } }
+                    }
+                })
+            },
+            {
+                title: "数码",
+                data: _.map(values[2], (entry, index) => {
+                    return {
+                        title: entry[index]["target"]["title"],
+                        onClick: () => { if (entry[index]["target"]["id"] != undefined) { here.openURL("https://www.zhihu.com/question/" + entry[index]["target"]["id"]) } }
+                    }
+                })
+            },
+            {
+                title: "体育",
+                data: _.map(values[3], (entry, index) => {
+                    return {
+                        title: entry[index]["target"]["title"],
+                        onClick: () => { if (entry[index]["target"]["id"] != undefined) { here.openURL("https://www.zhihu.com/question/" + entry[index]["target"]["id"]) } }
+                    }
+                })
+            },
+            {
+                title: "时尚",
+                data: _.map(values[4], (entry, index) => {
+                    return {
+                        title: entry[index]["target"]["title"],
+                        onClick: () => { if (entry[index]["target"]["id"] != undefined) { here.openURL("https://www.zhihu.com/question/" + entry[index]["target"]["id"]) } }
+                    }
+                })
+            },
+            {
+                title: "影视",
+                data: _.map(values[5], (entry, index) => {
+                    return {
+                        title: entry[index]["target"]["title"],
+                        onClick: () => { if (entry[index]["target"]["id"] != undefined) { here.openURL("https://www.zhihu.com/question/" + entry[index]["target"]["id"]) } }
+                    }
+                })
+            }
+        ]
+
+        here.popover.set(tabs)
+
+    });
 }
 
 here.on('load', () => {
     updateData()
     // Update every 2 hours
-    setInterval(updateData, 2*3600*1000)
+    setInterval(updateData, 2 * 3600 * 1000)
 })
 
 net.on('change', (type) => {
